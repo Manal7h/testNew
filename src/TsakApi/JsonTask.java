213adb509103d777f85e4f2125708159f8584678
manal7h <manal.alsenani22@gmail.com>
2022-11-29 02:37:00 -0800

    new project

---
+++ src/TsakApi/JsonTask.java
@@ -0,0 +1,24 @@
+package TsakApi;
+
+import java.io.Serializable;
+
+
+	public class JsonTask implements Serializable{
+		
+		private String fact;
+		private int length;
+		
+		
+		public String getFact() {
+			return fact;
+		}
+		public void setFact(String fact) {
+			this.fact = fact;
+		}
+		public int getLength() {
+			return length;
+		}
+		public void setLength(int length) {
+			this.length = length;
+		}
+}
--- src/TsakApi/TaskApi.java
+++ src/TsakApi/TaskApi.java
@@ -1,5 +1,61 @@
 package TsakApi;
 
-public class TaskApi {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
 
+public class TaskApi {
+	
+
+	  public static void main(String[] args) throws IOException {
+	
+		
+		System.out.println("Please Choice one Option:");
+		System.out.println("1- Consume API and Write File:");
+		System.out.println("3- Search word from File:");
+		System.out.println("4- Exite:");
+		
+		
+	
+		boolean isExit = true;
+		while (isExit) {
+			Scanner sc = new Scanner(System.in);
+			
+			int select = sc.nextInt();
+
+			switch (select) {
+			case 1:
+				try {
+			HttpClient client = HttpClient.newHttpClient();
+		    HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://catfact.ninja/fact"))
+			.build();
+		    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			System.out.println("My Json is : " + response.body());
+			String writeToFile = response.body();
+				FileWriter file = new FileWriter("TaskAPI.txt");
+				file.write(writeToFile.toString());
+				file.close();
+				} catch (Exception e) {
+				e.printStackTrace();
+			}
+				
+				System.out.println("JSON file created !!");
+				
+				break;
+
+			case 4:
+				System.out.println("Done");
+				System.exit(0);
+				break;
+
+			}
+
+		}
+		isExit = false;
+
+	}
 }
