2a8d819b077bb815fadbaeaed4b7134503771a14
manal7h <manal.alsenani22@gmail.com>
2022-12-06 02:59:39 -0800

    Creation of BufferedReader object

--- src/TsakApi/SearchJson.java
+++ src/TsakApi/SearchJson.java
@@ -10,15 +10,7 @@ import java.util.Scanner;
 public class SearchJson {
 	public static void main(String[] args) throws IOException{
 	Scanner sc = new Scanner(System.in);
-	
-	String[] words=null;  //Intialize the word Array
 
-	
-	
-	
-    FileReader fr = new FileReader("C:\\Users\\user018\\eclipse-workspace\\TASKAPI\\API.txt");  //Creation of File Reader object
-    BufferedReader br = new BufferedReader(fr); //Creation of BufferedReader object
-    String s; 
     
 
     System.out.println("Enter number of words:");
@@ -29,14 +21,16 @@ public class SearchJson {
     System.out.println("Enter number of words:" +num + "word :");
     
     for(int i=0 ; i<num; i++) {
-    	
-    	
-    	
+    	String[] words=null;//Intialize the word Array		
+    	FileReader fr = new FileReader("C:\\Users\\user018\\eclipse-workspace\\TASKAPI\\API.txt");  //Creation of File Reader object
+    	BufferedReader br = new BufferedReader(fr); //Creation of BufferedReader object
+    	int count=0;  //Intialize the word to zero
+    	String s; 
     	
     	
     }
 
-    int count=0;  //Intialize the word to zero
+    
     while((s=br.readLine())!=null)   //Reading Content from the file
     {
     	s=s.replaceAll("\\W", " ") ;
